1e72960d12e1e475aef08e5b94e05c5a511f165d
kragebein <stian.langvann@gmail.com>
2020-09-06 06:02:15 +0200

    Maybe this will work

--- pubgbot.py
+++ pubgbot.py
@@ -7,8 +7,8 @@ logging.basicConfig(level=logging.DEBUG)
 
 # Create an API key here: https://discordapp.com/developers/applications/
 # Create a webhook in the channel settings of your discord server
-bot_token = 'NjMyNzA4OTk4ODEzOTc0NTM4.Xaz-Aw.mO1vi-UsUmYymQ7wVPYjUxa1Aa0' # Bot token (discordapi)
-webhook_uri = 'https://discordapp.com/api/webhooks/672039725271613441/JWWMXhka6W9yfaTmMqpXYtFahaGTJ6gmKcSfPAiqvhvP9McAhH3Tp7zTMf4VD9Rapsyc' #Webhook URL (discord client)
+bot_token = 'CHANGEME' # Bot token (discordapi)
+webhook_uri = 'CHANGEME' #Webhook URL (discord client)
 
 check_rate = 10  # How often the bot will check for updates, in minutes (no less than five minutes or you'll be banned from pubg.report)
 if __name__ == "__main__":
